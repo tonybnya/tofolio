d3fc4aa541ab4a99eb0036acc3a9dc7c748a895c
Tony <nya.tony2010@gmail.com>
2023-01-31 09:13:03 +0100

    update

--- components/About.js
+++ components/About.js
@@ -5,13 +5,69 @@ function About() {
   return (
     <section id="about">
       <h1 className="title">about me</h1>
-      <div className="content flex justify-between">
+      <div className="content flex flex-col items-center">
         <div className="description">
           <h2>Get to know me</h2>
           <Link className="btn uppercase font-bold" href="/#contact">contact</Link>
         </div>
         <div className="skills">
           <h2>Main Skills</h2>
+          <div className="items">
+            <div className="skill">
+              <i className="fa-brands fa-html5"></i><span>HTML</span>
+            </div>
+            <div className="skill">
+              <i className="fa-brands fa-css3"></i><span>CSS</span>
+            </div>
+            <div className="skill">
+              <i className="fa-brands fa-sass"></i><span>SASS</span>
+            </div>
+            <div className="skill">
+              <span>Tailwind</span>
+            </div>
+            <div className="skill">
+              <i className="fa-brands fa-bootstrap"></i><span>Bootstrap</span>
+            </div>
+            <div className="skill">
+              <i className="fa-brands fa-python"></i><span>Python</span>
+            </div>
+            <div className="skill">
+              <i className="fa-brands fa-js"></i><span>JavaScript</span>
+            </div>
+            <div className="skill">
+              <span>C</span>
+            </div>
+            <div className="skill">
+              <span>Flask</span>
+            </div>
+            <div className="skill">
+              <span>Django</span>
+            </div>
+            <div className="skill">
+              <i className="fa-brands fa-react"></i><span>React</span>
+            </div>
+            <div className="skill">
+              <i className="fa-brands fa-angular"></i><span>Angular</span>
+            </div>
+            <div className="skill">
+              <i className="fa-brands fa-figma"></i><span>Figma</span>
+            </div>
+            <div className="skill">
+              <span>SQL</span>
+            </div>
+            <div className="skill">
+              <span>Firebase</span>
+            </div>
+            <div className="skill">
+              <span>MySQL</span>
+            </div>
+            <div className="skill">
+              <i className="fa-brands fa-git"></i><span>Git</span>
+            </div>
+            <div className="skill">
+              <span>Shell Bash</span>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,10 @@ export default function Home() {
         <title>Tony B. NYA | Portfolio</title>
         <meta name="description" content="Hi there! My name is Tony B. NYA. I'm a Software Engineer and a Web Developer from Cameroon. Let us get in touch!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="px-10 font-ms">
+      <main className="px-14 font-ms">
         <section className="min-h-screen">
           <Navbar />
           <Scrollbar />
